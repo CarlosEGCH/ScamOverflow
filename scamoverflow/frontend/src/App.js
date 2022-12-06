fdeb0ffb4d0cd4a679fa9b0c86552b8ae92029ed
CarlosEGCH <carlitosgch4@gmail.com>
2022-12-06 16:06:55 +0000

    Desktop navbar finished

--- scamoverflow/frontend/src/App.js
+++ scamoverflow/frontend/src/App.js
@@ -1,3 +1,5 @@
+import "./styles/App.css"
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
--- scamoverflow/frontend/src/components/Navbar.js
+++ scamoverflow/frontend/src/components/Navbar.js
@@ -1,6 +1,12 @@
 
+import "../styles/Navbar.css"
 import { useViewport } from '../hooks/responsive.js';
 
+import messageIcon from "../assets/message-square-plus.svg"
+import ticketIcon from "../assets/mail-03.svg"
+import bellIcon from "../assets/bell-03.svg"
+import userIcon from "../assets/user-profile-02.svg"
+import pageLogo from "../assets/scamoverflowlogo.png"
 
 export default function Navbar(){
 
@@ -8,8 +14,35 @@ export default function Navbar(){
 
     return(
         <>
-            <div>
-                <p>Navbar! {width > 900 ? "Desktop" : "Mobile"}</p>
+            <div className="navbar-wrapper">
+                <div className="navbar-container">
+                    <div className="navbar-logo">
+                        <img src={pageLogo} />
+                        <p>ScamOverflow</p>
+                    </div>
+                    <div className="navbar-links">
+                        <ul>
+                            <li>
+                                <img src={messageIcon} />
+                                <p>Report Scam</p>
+                            </li>
+                            <li>
+                                <img src={ticketIcon} />
+                                <p>Tickets</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navbar-options">
+                        <ul>
+                            <li>
+                                <img src={bellIcon} />
+                            </li>
+                            <li>
+                                <img src={userIcon} />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </>
     )
